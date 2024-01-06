22b7dd146e35009b5c83927dde00f5c1790dabfa
CharmingHue <1299670883@qq.com>
2024-01-06 16:07:35 +0800

    Add emablock in fast neck

--- models/neck/__init__.py
+++ models/neck/__init__.py
@@ -3,7 +3,8 @@ from .fpem_v2 import FPEM_v2
 from .fpn import FPN
 from .fast_neck import fast_neck
 from .fast_neck_asf import fast_neck_asf
+from .fast_neck_ema import fast_neck_ema
 from .builder import build_neck
 
 
-__all__ = ['FPEM_v1', 'FPEM_v2', 'fast_neck', 'fast_neck_asf', 'FPN', 'build_neck']
+__all__ = ['FPEM_v1', 'FPEM_v2', 'fast_neck', 'fast_neck_asf','fast_neck_ema', 'FPN', 'build_neck']
