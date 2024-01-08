fec2a2249114db7cbdf9c6083bab4af30999ee82
CharmingHue <1299670883@qq.com>
2024-01-08 11:34:00 +0800

    Optimize fsnet ema neck

--- models/neck/fast_neck_ema.py
+++ models/neck/fast_neck_ema.py
@@ -7,7 +7,7 @@ import json
 
 
 class FASTNeck(nn.Module):
-    def __init__(self, reduce_layer1, reduce_layer2, reduce_layer3, reduce_layer4, same=False):
+    def __init__(self, reduce_layer1, reduce_layer2, reduce_layer3, reduce_layer4, same=False, fsnet=False):
         super(FASTNeck, self).__init__()
         self.reduce_layer1 = reduce_layer1
         self.reduce_layer2 = reduce_layer2
@@ -15,8 +15,11 @@ class FASTNeck(nn.Module):
         self.reduce_layer4 = reduce_layer4
         
         self.same = same
+        self.fsnet = fsnet
         if self.same:
             self.emablock = EMA(128)
+        elif self.fsnet:
+            self.emablock = EMA(256)
         else:
             self.emablock1 = EMA(64)
             self.emablock2 = EMA(128)
@@ -39,7 +42,13 @@ class FASTNeck(nn.Module):
     
     def forward(self, x):
         f1, f2, f3, f4 = x
-        if not self.same:
+        
+        if self.fsnet:
+            f1 = self.emablock(f1)
+            f2 = self.emablock(f2)
+            f3 = self.emablock(f3)
+            f4 = self.emablock(f4)
+        elif not self.same:
             f1 = self.emablock1(f1)
             f2 = self.emablock2(f2)
             f3 = self.emablock3(f3)
@@ -63,12 +72,12 @@ class FASTNeck(nn.Module):
         return f
 
     @staticmethod
-    def build_from_config(config, same=False):
+    def build_from_config(config, same=False, fsnet=False):
         reduce_layer1 = set_layer_from_config(config['reduce_layer1'])
         reduce_layer2 = set_layer_from_config(config['reduce_layer2'])
         reduce_layer3 = set_layer_from_config(config['reduce_layer3'])
         reduce_layer4 = set_layer_from_config(config['reduce_layer4'])
-        return FASTNeck(reduce_layer1, reduce_layer2, reduce_layer3, reduce_layer4, same)
+        return FASTNeck(reduce_layer1, reduce_layer2, reduce_layer3, reduce_layer4, same, fsnet)
     
     
 def fast_neck_ema(config, **kwargs):
@@ -79,4 +88,9 @@ def fast_neck_ema(config, **kwargs):
 def fast_neck_ema_same(config, **kwargs):
     neck_config = json.load(open(config, 'r'))['neck']
     neck = FASTNeck.build_from_config(neck_config, True, **kwargs)
+    return neck
+
+def fast_neck_fsnet_ema(config, **kwargs):
+    neck_config = json.load(open(config, 'r'))['neck']
+    neck = FASTNeck.build_from_config(neck_config, False, True, **kwargs)
     return neck
