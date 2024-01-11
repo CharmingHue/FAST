ae9b9056236d76cb78676cead9aeebefde3c0768
CharmingHue <1299670883@qq.com>
2024-01-11 22:30:32 +0800

    Optimize compile

--- compile.sh
+++ compile.sh
@@ -1,7 +1,31 @@
+#!/bin/bash
+setup_env=${1}
+
+if [ "$setup_env" = "True" ]; then
+    python -m pip install --upgrade pip
+    pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
+    pip install editdistance
+    pip install Polygon3
+    pip install pyclipper
+    pip install Cython
+    pip install openmim
+    mim install mmcv==1.7.2
+    pip install prefetch_generator
+    pip install scipy
+    pip install yacs
+    pip install tqdm
+    pip install opencv-python==4.6.0.66
+fi
+
 cd ./models/post_processing/pa/
-python3 setup.py build_ext --inplace
-cd ../pse/
-python3 setup.py build_ext --inplace
-cd ../ccl/
-python3 setup.py build_ext --inplace
+python setup.py build_ext --inplace
 cd ../../../
+
+cd ./models/post_processing/pse/
+python setup.py build_ext --inplace
+cd ../../../
+
+cd ./models/post_processing/ccl/
+python setup.py build_ext --inplace
+cd ../../../
+
