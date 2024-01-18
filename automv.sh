ed8c44a8d51346dcd409d0b8b0ab932362a058b4
CharmingHue <1299670883@qq.com>
2024-01-18 15:56:26 +0800

    Optimize automv

--- automv.sh
+++ automv.sh
@@ -3,14 +3,19 @@ config=${1}
 wait_time=${2:-3600}
 
 outpath="$HOME/autodl-fs/$config"
+datapath="$HOME/autodl-tmp/$config"
 if [ ! -d $outpath ]; then
     mkdir -p $outpath
     echo "成功创建文件夹"
+    # 执行文件移动操作
 else
     echo "文件夹已存在"
 fi
 # 初始化计数器
 count_mv=0
+cp ~/FAST/checkpoints/$config/*.tar $datapath
+mv ~/FAST/checkpoints/$config/*.tar $outpath
+echo "成功移动权重文件"
 
 while :
 do
@@ -20,6 +25,7 @@ do
     # 检查是否达到等待时间
     if [ $count_mv -ge $wait_time ]; then
         # 执行文件移动操作
+        cp ~/FAST/checkpoints/$config/*.tar $datapath
         mv ~/FAST/checkpoints/$config/*.tar $outpath
         echo "成功移动权重文件"
         # 重置计数器
